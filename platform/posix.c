82e4df4ed4ae73b9e391dc250d25a524d69a7598
Johannes Kliemann <kliemann@componolit.com>
2018-11-26 09:57:16 +0100

    add gcc runtime exception fixes #15

--- platform/posix.c
+++ platform/posix.c
@@ -1,3 +1,14 @@
+/*
+ * Copyright (C) 2018 Componolit GmbH
+ *
+ * This file is part of the Componolit Ada runtime, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ *
+ * As a special exception under Section 7 of GPL version 3, you are granted
+ * additional permissions described in the GCC Runtime Library Exception,
+ * version 3.1, as published by the Free Software Foundation.
+ */
+
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
--- src/lib/platform.adb
+++ src/lib/platform.adb
@@ -1,3 +1,12 @@
+--  Copyright (C) 2018 Componolit GmbH
+--
+--  This file is part of the Componolit Ada runtime, which is distributed
+--  under the terms of the GNU Affero General Public License version 3.
+--
+--  As a special exception under Section 7 of GPL version 3, you are granted
+--  additional permissions described in the GCC Runtime Library Exception,
+--  version 3.1, as published by the Free Software Foundation.
+
 package body Platform
   with SPARK_Mode => Off
 is
