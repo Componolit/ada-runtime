ce1502300dda3d28179113a575895e7a260efbce
Alexander Senier <senier@componolit.com>
2019-03-06 16:55:39 +0100

    Support exit status
    Ref. #22

--- src/lib/exit.c
+++ src/lib/exit.c
@@ -1,6 +1,11 @@
+/* this symbol is required/expected by the binder */
 int gnat_exit_status = 0;
+
+/* this symbol is required/expected by the runtime */
+int __ada_runtime_exit_status = 0;
 
 void __gnat_set_exit_status (int value)
 {
    gnat_exit_status = value;
+   __ada_runtime_exit_status = value;
 };
