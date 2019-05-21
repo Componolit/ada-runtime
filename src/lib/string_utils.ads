c7df0341fc8ace8e84052700d6d02235449501bc
Johannes Kliemann <kliemann@componolit.com>
2019-05-21 14:51:51 +0200

    style fixes

--- src/lib/string_utils.ads
+++ src/lib/string_utils.ads
@@ -12,29 +12,27 @@ with System.Storage_Elements;
 use all type System.Address;
 use all type System.Storage_Elements.Integer_Address;
 
-package String_Utils
-with SPARK_Mode
+package String_Utils with
+   SPARK_Mode
 is
 
    pragma Preelaborate;
 
    function Length (C_Str      : System.Address;
-                    Max_Length : Natural := Natural'Last) return Integer
-     with
-       Post => Length'Result <= Max_Length,
-       Contract_Cases =>
+                    Max_Length : Natural := Natural'Last) return Integer with
+      Post => Length'Result <= Max_Length,
+      Contract_Cases =>
          (C_Str = System.Null_Address  => Length'Result <= 0,
           C_Str /= System.Null_Address => Length'Result >= 0);
    pragma Annotate (GNATprove, Terminating, Length);
 
    function Convert_To_Ada (C_Str      : System.Address;
                             Default    : String;
-                            Max_Length : Natural :=
-                              Natural'Last) return String
-       with
-         Contract_Cases =>
-           (C_Str = System.Null_Address => Convert_To_Ada'Result = Default,
-            C_Str /= System.Null_Address => True);
+                            Max_Length : Natural := Natural'Last)
+                            return String with
+      Contract_Cases =>
+         (C_Str = System.Null_Address => Convert_To_Ada'Result = Default,
+          C_Str /= System.Null_Address => True);
    pragma Annotate (GNATprove, Terminating, Convert_To_Ada);
 
 private
@@ -52,24 +50,21 @@ private
    function Get_Char (Ptr : Valid_Pointer) return Character;
    pragma Annotate (GNATprove, Terminating, Get_Char);
 
-   function Incr (Ptr : Valid_Pointer) return Valid_Pointer
-     with
-       Pre => Ptr < Pointer'Last,
-       Post => Incr'Result = Ptr + 1;
+   function Incr (Ptr : Valid_Pointer) return Valid_Pointer with
+      Pre => Ptr < Pointer'Last,
+      Post => Incr'Result = Ptr + 1;
    pragma Annotate (GNATprove, Terminating, Incr);
 
-   function To_Address (Value : Pointer) return System.Address
-     with
-       Inline,
-       Contract_Cases =>
+   function To_Address (Value : Pointer) return System.Address with
+      Inline,
+      Contract_Cases =>
          (Value = Null_Pointer  => To_Address'Result = System.Null_Address,
           Value /= Null_Pointer => To_Address'Result /= System.Null_Address);
    pragma Annotate (GNATprove, Terminating, To_Address);
 
-   function To_Pointer (Addr : System.Address) return Pointer
-     with
-       Inline,
-       Contract_Cases =>
+   function To_Pointer (Addr : System.Address) return Pointer with
+      Inline,
+      Contract_Cases =>
          (Addr = System.Null_Address  => To_Pointer'Result = Null_Pointer,
           Addr /= System.Null_Address => To_Pointer'Result /= Null_Pointer);
    pragma Annotate (GNATprove, Terminating, To_Pointer);
