3b5e318d3cf9feb3c64630b21c15baf72bab2f91
Johannes Kliemann <kliemann@componolit.com>
2019-07-18 18:25:26 +0200

    unit tests for conversion, update muen test fixes #47

--- tests/unit/rts_suite.adb
+++ tests/unit/rts_suite.adb
@@ -1,11 +1,13 @@
 with Componolit.Runtime.Strings.Tests;
+with Componolit.Runtime.Conversions.Tests;
 
 package body Rts_Suite is
    use Aunit.Test_Suites;
 
    Result : aliased Test_Suite;
 
-   Strings_Case : aliased Componolit.Runtime.Strings.Tests.Test_Case;
+   Strings_Case     : aliased Componolit.Runtime.Strings.Tests.Test_Case;
+   Conversions_Case : aliased Componolit.Runtime.Conversions.Tests.Test_Case;
 
    -----------
    -- Suite --
@@ -14,6 +16,7 @@ package body Rts_Suite is
    function Suite return AUnit.Test_Suites.Access_Test_Suite is
    begin
       Result.Add_Test (Strings_Case'Access);
+      Result.Add_Test (Conversions_Case'Access);
       return Result'Access;
    end Suite;
 
