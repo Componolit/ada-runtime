5fd32116ba89eef8aae271b5a0d95f4602492274
Johannes Kliemann <kliemann@componolit.com>
2019-07-19 12:21:34 +0200

    prove lemma unsigned associativity ref #50

--- src/lib/componolit-runtime-conversions.adb
+++ src/lib/componolit-runtime-conversions.adb
@@ -24,7 +24,79 @@ is
 
    procedure Lemma_Identity (I : Int64; U : Uns64) is null;
 
-   procedure Lemma_Uns_Associativity_Add (X, Y : Int64) is null;
+   procedure Lemma_Uns_Associativity_Add (X, Y : Int64)
+   is
+   begin
+      if X < 0 and Y < 0 then
+         if X = Int64'First and Y = Int64'First then
+            pragma Assert (X + Y = To_Int (To_Uns (X) + To_Uns (Y)));
+         elsif X = Int64'First then
+            pragma Assert (X + Y = To_Int (To_Uns (X) + To_Uns (Y)));
+         elsif Y = Int64'First then
+            pragma Assert (X + Y = To_Int (To_Uns (X) + To_Uns (Y)));
+         else
+            declare
+               Sum_Int : constant Int64 :=
+                  To_Int (To_Uns (X)) + To_Int (To_Uns (Y));
+               Sum_Uns : constant Int64 := To_Int (To_Uns (X) + To_Uns (Y));
+            begin
+               pragma Assert (To_Uns (X) = -Uns64 (-X));
+               pragma Assert (To_Uns (Y) = -Uns64 (-Y));
+               pragma Assert (-Uns64 (-X) - Uns64 (-Y) = -Uns64 (-X - Y));
+               pragma Assert (-Uns64 (-X - Y) > Uns64 (Int64'Last));
+               pragma Assert (Sum_Uns = To_Int (-Uns64 (-X) - Uns64 (-Y)));
+               pragma Assert (Sum_Uns = To_Int (-Uns64 (-X - Y)));
+               pragma Assert (Sum_Uns = -Int64 (Uns64'Last +
+                                                Uns64 (-X - Y)) - 1);
+               pragma Assert (Uns64'Last + Uns64 (-X - Y) =
+                                 Uns64 (-X - Y - 1));
+               pragma Assert (Sum_Uns = -Int64 (-X - Y - 1) - 1);
+               pragma Assert (Sum_Uns = X + Y);
+               pragma Assert (Sum_Int = Sum_Uns);
+            end;
+         end if;
+      elsif X >= 0 and Y >= 0 then
+         pragma Assert (X + Y = To_Int (To_Uns (X) + To_Uns (Y)));
+      elsif X >= 0 and Y < 0 then
+         if Y = Int64'First then
+            pragma Assert (X + Y = To_Int (To_Uns (X) + To_Uns (Y)));
+         else
+            declare
+               Sum_Int : constant Int64 :=
+                  To_Int (To_Uns (X)) + To_Int (To_Uns (Y));
+               Sum_Uns : constant Int64 := To_Int (To_Uns (X) + To_Uns (Y));
+            begin
+               pragma Assert (To_Uns (Y) = -Uns64 (-Y));
+               pragma Assert (To_Uns (X) = Uns64 (X));
+               pragma Assert (Sum_Uns = To_Int (Uns64 (X) - Uns64 (-Y)));
+               pragma Assert (Sum_Int = Sum_Uns);
+               pragma Assert (X + Y = To_Int (To_Uns (X) + To_Uns (Y)));
+            end;
+         end if;
+      elsif X < 0 and Y >= 0 then
+         if X = Int64'First then
+            pragma Assert (X + Y = To_Int (To_Uns (X) + To_Uns (Y)));
+         else
+            declare
+               Sum_Int : constant Int64 :=
+                  To_Int (To_Uns (Y)) + To_Int (To_Uns (X));
+               Sum_Uns : constant Int64 := To_Int (To_Uns (Y) + To_Uns (X));
+            begin
+               pragma Assert (To_Uns (X) = -Uns64 (-X));
+               pragma Assert (To_Uns (Y) = Uns64 (Y));
+               pragma Assert (Sum_Uns = To_Int (Uns64 (Y) - Uns64 (-X)));
+               pragma Assert (To_Uns (X) + To_Uns (Y) =
+                                 To_Uns (Y) + To_Uns (X));
+               pragma Assert (To_Int (To_Uns (Y)) + To_Int (To_Uns (X)) =
+                                 To_Int (To_Uns (Y) + To_Uns (X)));
+               pragma Assert (Sum_Int = Sum_Uns);
+               pragma Assert (X + Y = To_Int (To_Uns (X) + To_Uns (Y)));
+            end;
+         end if;
+      else
+         pragma Assert (X + Y = To_Int (To_Uns (X) + To_Uns (Y)));
+      end if;
+   end Lemma_Uns_Associativity_Add;
 
    procedure Lemma_Uns_Associativity_Sub (X, Y : Int64) is null;
 
