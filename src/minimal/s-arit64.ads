36e6549852958faaea9aab25a71dc4775afba98f
Johannes Kliemann <kliemann@componolit.com>
2019-07-19 15:57:01 +0200

    add dependencies to add and subtract

--- src/minimal/s-arit64.ads
+++ src/minimal/s-arit64.ads
@@ -47,17 +47,21 @@ is
    use type Interfaces.Integer_64;
 
    function Add_With_Ovflo_Check (X, Y : Int64) return Int64 with
-      Pre  => (if X < 0 and Y <= 0 then Int64'First - X < Y)
-              and (if X >= 0 and Y >= 0 then Int64'Last - X >= Y),
-      Post => Add_With_Ovflo_Check'Result = X + Y;
+      Pre     => (if X < 0 and Y <= 0 then Int64'First - X < Y)
+                 and (if X >= 0 and Y >= 0 then Int64'Last - X >= Y),
+      Post    => Add_With_Ovflo_Check'Result = X + Y,
+      Global  => null,
+      Depends => (Add_With_Ovflo_Check'Result => (X, Y));
    --  Raises Constraint_Error if sum of operands overflows 64 bits,
    --  otherwise returns the 64-bit signed integer sum.
 
    function Subtract_With_Ovflo_Check (X, Y : Int64) return Int64 with
-      Pre  => (if X >= 0 and Y <= 0 then Y > Int64'First
-                  and then Int64'Last - X >= abs (Y))
-              and (if X < 0 and Y > 0 then Y < Int64'First - X),
-      Post => Subtract_With_Ovflo_Check'Result = X - Y;
+      Pre     => (if X >= 0 and Y <= 0 then Y > Int64'First
+                     and then Int64'Last - X >= abs (Y))
+                 and (if X < 0 and Y > 0 then Y < Int64'First - X),
+      Post    => Subtract_With_Ovflo_Check'Result = X - Y,
+      Global  => null,
+      Depends => (Subtract_With_Ovflo_Check'Result => (X, Y));
    --  Raises Constraint_Error if difference of operands overflows 64
    --  bits, otherwise returns the 64-bit signed integer difference.
 
