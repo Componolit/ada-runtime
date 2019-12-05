a81aef176e7f9e6f741301d625c52a1abc8c090d
Johannes Kliemann <kliemann@componolit.com>
2019-12-05 17:45:04 +0100

    run dispatching test on genode
    fixes #59

--- tests/genode.sh
+++ tests/genode.sh
@@ -6,7 +6,7 @@ ln -sf /app /genode/contrib/ada-runtime-test/ada-runtime
 cd /genode
 git remote add jklmnn https://github.com/jklmnn/genode.git
 git fetch --all
-git checkout 3f0651588ed1c2d432b24276f48a0fc6c8785eb9
+git checkout cf9827f313b912245b149406e54095a4c2c3843c
 ./tool/create_builddir x86_64
 ./tool/ports/prepare_port ada-runtime gcov
 ln -sf /genode/contrib/ada-runtime-$(cat /genode/repos/libports/ports/ada-runtime.hash)/ada-runtime-alis /genode/contrib/ada-runtime-test/
@@ -16,9 +16,11 @@ sed -i "s/^#REPOS/REPOS/g;s/^#MAKE.*$/MAKE += -j$(nproc)/g" etc/build.conf
 make run/test KERNEL=linux BOARD=linux PKG=test-spark
 make run/test KERNEL=linux BOARD=linux PKG=test-spark_exception
 make run/test KERNEL=linux BOARD=linux PKG=test-spark_secondary_stack
+make run/test KERNEL=linux BOARD=linux PKG=test-spark_dispatching
 /genode/tool/depot/create -j$(nproc) UPDATE_VERSIONS=1 FORCE_BUILD=1 REBUILD=1 \
     genodelabs/pkg/x86_64/test-spark genodelabs/pkg/x86_64/test-spark_exception \
     genodelabs/pkg/x86_64/test-spark_secondary_stack \
+    genodelabs/pkg/x86_64/test-spark_dispatching \
     genodelabs/bin/x86_64/depot_query \
     genodelabs/bin/x86_64/fs_rom \
     genodelabs/bin/x86_64/loader \
@@ -28,4 +30,4 @@ make run/test KERNEL=linux BOARD=linux PKG=test-spark_secondary_stack
     genodelabs/bin/x86_64/base-linux \
     genodelabs/bin/x86_64/report_rom \
     CROSS_DEV_PREFIX=/usr/local/genode/tool/19.05/bin/genode-x86-
-make KERNEL=linux BOARD=linux run/depot_autopilot TEST_PKGS="test-spark test-spark_secondary_stack test-spark_exception"
+make KERNEL=linux BOARD=linux run/depot_autopilot TEST_PKGS="test-spark test-spark_secondary_stack test-spark_exception test-spark_dispatching"
