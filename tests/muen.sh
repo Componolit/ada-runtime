45441f1f2cf2ebb4f0963b9404ffba02f91c9a3d
Johannes Kliemann <kliemann@componolit.com>
2019-12-05 16:07:17 +0100

    update muen port to build with tag support
    ref #59

--- tests/muen.sh
+++ tests/muen.sh
@@ -3,7 +3,7 @@
 cd /muen
 git reset --hard
 git fetch --all
-git checkout 82250b20144be95bc0af4ba266035c24042acccf
+git checkout 0b00af04d5de485c4bbb9aff2393472a180e7499
 rm -r components/spark_runtime/src
 ln -sf /app components/spark_runtime/src
 make -j$(nproc)
