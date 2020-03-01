4f55430eee388271aad0bcf946a7d6932b5d8afd
Johannes Kliemann <kliemann@componolit.com>
2020-03-01 15:29:26 +0100

    test esp8266 target

---
+++ tests/esp8266.sh
@@ -0,0 +1,8 @@
+
+set -e
+
+wget -q https://github.com/jklmnn/gnat-llvm-xtensa/releases/download/20.2-20200228/gnat-llvm-xtensa.tar.xz
+tar xvf gnat-llvm-xtensa.tar.xz
+export PATH=/gnat-llvm-xtensa/bin:$PATH
+cd /app
+make esp8266
