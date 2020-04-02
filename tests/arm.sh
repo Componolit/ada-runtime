4e029bd4f6f2b41d9eba4fbdbbe1035584de5383
Johannes Kliemann <jk@jkliemann.de>
2020-04-02 15:41:58 +0200

    test all arm boards
    ref #61

--- tests/arm.sh
+++ tests/arm.sh
@@ -4,5 +4,12 @@ set -e
 cd /app
 make stm32f0
 gprbuild -P tests/platform/stm32f0/test.gpr
-make nrf52
-gprbuild -P tests/platform/nrf52/test.gpr
+gprclean -P tests/platform/stm32f0/test.gpr
+make clean
+for board in default bluefruit_feather sparkfun
+do
+    make nrf52 BOARD=$board
+    gprbuild -P tests/platform/nrf52/test.gpr
+    gprclean -P tests/platform/nrf52/test.gpr
+    make clean
+done
