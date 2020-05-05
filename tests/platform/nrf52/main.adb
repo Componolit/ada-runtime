f8d93972888682b86522b06f138757ca410b1d98
Johannes Kliemann <kliemann@componolit.com>
2020-05-05 19:00:03 +0200

    Prove GPIO configuration properties

--- tests/platform/nrf52/main.adb
+++ tests/platform/nrf52/main.adb
@@ -1,6 +1,14 @@
+with Componolit.Runtime.Drivers.GPIO;
 
-procedure Main
+procedure Main with
+   SPARK_Mode
 is
+   package GPIO renames Componolit.Runtime.Drivers.GPIO;
+   use type GPIO.Mode;
 begin
-   null;
+   GPIO.Configure (15, GPIO.Port_In);
+   pragma Assert (GPIO.Pin_Mode (15) = GPIO.Port_In);
+   GPIO.Configure (16, GPIO.Port_In);
+   pragma Assert (GPIO.Pin_Mode (15) = GPIO.Port_In);
+   pragma Assert (GPIO.Pin_Mode (16) = GPIO.Port_In);
 end Main;
--- tests/platform/stm32f0/main.adb
+++ tests/platform/stm32f0/main.adb
@@ -1,6 +1,14 @@
+with Componolit.Runtime.Drivers.GPIO;
 
-procedure Main
+procedure Main with
+   SPARK_Mode
 is
+   package GPIO renames Componolit.Runtime.Drivers.GPIO;
+   use type GPIO.Mode;
 begin
-   null;
+   GPIO.Initialize;
+   GPIO.Configure (GPIO.PC8, GPIO.Port_Out);
+   GPIO.Configure (GPIO.PC9, GPIO.Port_Out);
+   pragma Assert (GPIO.Pin_Mode (GPIO.PC8) = GPIO.Port_Out);
+   pragma Assert (GPIO.Pin_Mode (GPIO.PC9) = GPIO.Port_Out);
 end Main;
