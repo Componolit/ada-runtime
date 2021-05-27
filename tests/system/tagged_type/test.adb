f2407fcdf3faaa6a9ea5c9a259f526f16d6174d3
Johannes Kliemann <kliemann@componolit.com>
2021-05-27 10:53:13 +0200

    Add support for GNAT Community 2021
    ref #80

--- tests/system/tagged_type/test.adb
+++ tests/system/tagged_type/test.adb
@@ -17,6 +17,9 @@ begin
       or else O_C.Is_Parent
       or else not Is_Parent (O_P)
       or else Is_Parent (O_C)
+      or else O_P not in Parent.Object'Class
+      or else O_C not in Parent.Object'Class
+      or else O_C not in Child.Object'Class
    then
       raise Program_Error;
    end if;
